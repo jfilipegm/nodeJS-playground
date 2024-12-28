f35de1b6150dead2c808c674599858ab552a3e0c
Filipe <joaomendonca2000@hotmail.com>
2024-12-28 19:03:46 +0000

    Commiting cuz api limit was reached for the day. Added some dependecies, tried to work around the pay wall of the API and kinda setup a basic live display of lineups and score for primeira Liga

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-import { mercury as mercuryESmodules } from "./moduleMercury.js";
+import express from "express";
+import { getFixtures, getFixtureLineups } from "./service.js";
+import dotenv from "dotenv";
 
-//const mercury = require("./moduleMercury");
+dotenv.config();
 
-//console.log(mercury);
-console.log(mercuryESmodules);
+const app = express();
+app.set("view engine", "ejs");
+
+app.get("/", async (request, response) => {
+  try {
+    const fixtureData = await getFixtures();
+    const liveFixtures = fixtureData?.response || [];
+    const fixtureId = liveFixtures[0].fixture.id;
+    const fixtureLineups = await getFixtureLineups(fixtureId);
+
+    console.log(fixtureLineups);
+
+    response.render("home", { liveFixtures, fixtureLineups });
+  } catch (err) {
+    console.error(err);
+    response.status(500).send("Sorry, out of order");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000 @ http://localhost:3000");
+});
---
+++ service.js
@@ -0,0 +1,63 @@
+import axios from "axios";
+
+export const getLeagues = async () => {
+  const config = {
+    method: "get",
+    url: "https://v3.football.api-sports.io/leagues",
+    headers: {
+      "x-rapidapi-key": process.env.API_KEY,
+      "x-rapidapi-host": "v3.football.api-sports.io",
+    },
+  };
+
+  try {
+    const response = await axios(config);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getFixtures = async () => {
+  const today = new Date();
+  const dateString = today.toISOString().split("T")[0];
+  const config = {
+    method: "get",
+    url: "https://v3.football.api-sports.io/fixtures",
+    params: {
+      live: "all",
+      league: 94,
+    },
+    headers: {
+      "x-rapidapi-key": process.env.API_KEY,
+      "x-rapidapi-host": "v3.football.api-sports.io",
+    },
+  };
+
+  try {
+    const response = await axios(config);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const getFixtureLineups = async (fixtureId) => {
+  const config = {
+    method: "get",
+    url: "https://v3.football.api-sports.io/fixtures/lineups",
+    params: { fixture: fixtureId },
+    headers: {
+      "x-rapidapi-key": process.env.API_KEY,
+      "x-rapidapi-host": "v3.football.api-sports.io",
+    },
+  };
+
+  try {
+    const response = await axios(config);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
